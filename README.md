3fa3a8c95231bf47d26a95bef719f156ce93b171
stefannyJAleman <stefannyjal3man@gmail.com>
2023-10-23 17:32:50 -0500

    note.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-
+https://www.youtube.com/watch?v=EtSre3I87Fg&ab_channel=Inducci%C3%B3nFormaci%C3%B3nVirtual
--- app/src/main/java/com/example/reto2/Inicio.java
+++ app/src/main/java/com/example/reto2/Inicio.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+// Stefanny Jaleman
 public class Inicio extends AppCompatActivity {
 
     @Override
@@ -15,6 +15,7 @@ public class Inicio extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Crear botones
+
 
         Button btnregitrarse=findViewById(R.id.btnRegistroInicio);
         Button btnLoginInicio=findViewById(R.id.btnLoginInicio);
